cc48885530dc65bd8428244c7cd4f1fd4461d2f1
VadimTabelskij <bagum94@gmail.com>
2023-02-20 18:00:27 +0200

    feat: added cars model and img  install axios

---
+++ client/src/models/cars-model.d.ts
@@ -0,0 +1,11 @@
+type CarsModel = {
+
+  id: string,
+  brands: string,
+  location: {
+    country: string,
+    city: string,
+  },
+  images: string[],
+  style : string,
+};
---
+++ client/src/services/api-service.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5024',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+const fetchCars = async () => {
+  const response = await api.get<CarsModel[]>('/cars');
+
+  return response.data;
+};
+
+const ApiService = {
+  fetchCars,
+};
+
+export default ApiService;
